43ab61824760ffddc8948e85afd723e90e797d95
akmnats <31035959+akmnats@users.noreply.github.com>
2017-08-17 01:59:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'elephant.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'ui/elephant.png'));
 });
 
 
